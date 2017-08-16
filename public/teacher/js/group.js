d63cdc5472a0abc2f33bcaf64e8d10e6242903fd
Anurag-545 <ranaasr60@gmail.com>
2017-08-17 01:39:46 +0530

    delete user

--- public/teacher/js/group.js
+++ public/teacher/js/group.js
@@ -8,14 +8,14 @@ appPreview.config(['$locationProvider', function($locationProvider){
 }]);
 appPreview.controller('groupCtrl',['$scope','$http','$location','$filter',function($scope,$http,$location,$filter){
   $scope.examid=$location.search().examid;
-  $scope.exam={};
-  $scope.questions={};
   $scope.users=[];
   $scope.entries="";
   $scope.searchText = null;
+  $scope.groupName="";
   $scope.change = function(text) {
       if($scope.searchText && $scope.searchText.length>3){
-          /*   $("#search").fadeIn("fast"); */
+           $(".search").fadeIn("fast");
+             $scope.msg="";
       $http.get('/teacher/getuser?users=' + $scope.searchText).then(function(response){
           $scope.entries= response.data;
         });
@@ -24,19 +24,52 @@ appPreview.controller('groupCtrl',['$scope','$http','$location','$filter',functi
            $scope.entries="";
        }
     };
-  /*  $(document).click( function(){
-      $('#search').hide();
-    });*/
-      $scope.msg="";
+
+  $(document).click( function(){
+      $('.search').hide();
+    });
+
   $scope.User=function(user){
     var newTemp = $filter("filter")($scope.users, {email:user.email}, true);
-    console.log(newTemp.length);
     if(newTemp.length==0){
     $scope.users.push(user);
+      $scope.searchText = null;
     }
     else if(!newTemp.length==0){
       $scope.msg="Already added";
+        $scope.searchText = null;
     }
   }
+  $scope.remove=function(index){
+    $scope.users.splice(index,1);
+  }
 
+  $scope.addgroup=function(){
+   if($scope.searchText && $scope.users.length!=0){
+     var array=[];
+     foreach(user in users){
+       array.push(user.email);
+     }
+     var groupObj={
+       groupName:$scope.groupName,
+       users:array
+     }
+     console.log(groupObj);
+     $http.post('/teacher/addgroup',groupObj).then(function(response){
+      console.log(response.data);
+      //console.log("done");
+       //$log.log($scope.examid);
+       window.location="/teacher/group#groups";
+     },function(error){
+      console.log("error in group http");
+     });
+
+   }else{
+     if(!$scope.searchText){
+     $scope.error="Plz add group Name";}
+     if($scope.users.length!=0){
+       $scope.error="PLz add atleast one student";
+     }
+   }
+  }
 }]);
