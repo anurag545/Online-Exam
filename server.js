11dcc3e51219181f80ce7e057efab8d93f8ee89f
Anurag-545 <ranaasr60@gmail.com>
2017-09-06 10:57:36 +0530

    update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var mongoose=require('mongoose');
  //var User=require('./schemas/userSchema.js');
  //var Exam=require('./admin/js/examSchema.js');
   //var Question=require('./admin/js/quesSchema.js');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/onlineExam",{useMongoClient: true});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/onlineExam",{useMongoClient: true});
+mongoose.connect("mongodb://localhost:27017/onlineExam",{useMongoClient: true});
 
 var bodyParser=require('body-parser');
 app.use(bodyParser.json({strict:false}));
